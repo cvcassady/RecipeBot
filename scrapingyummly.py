558b1edd39ed3a4c51f9c0884a3fc9225f67f62f
Colin Cassady <cvcassady@gmail.com>
2017-01-28 09:17:35 -0500

    Scraper scraping

--- scrapingyummly.py
+++ scrapingyummly.py
@@ -1,29 +1,77 @@
 import datetime
 import json
+from json import JSONDecodeError
 import urllib.request
+from urllib.error import HTTPError
 import sqlite3
 import os
+import sys
 
-os.chdir(r"C:\Users\Colin\Documents\Vision And Language\yummly")
+#os.chdir(r"C:\Users\Colin\Documents\Vision And Language\RecipeBot")
 
 
 current = 0
 rate = 500
+query = "recipe"
 
-url = r"https://mapi.yummly.com/mapi/v13/content/search?q=recipe&start=" + str(current) + r"&maxResult=" + str(rate) + r"&fetchUserCollections=false&allowedContent[]=single_recipe&allowedContent[]=suggested_source&allowedContent[]=suggested_search&allowedContent[]=related_search&facetField[]=diet&facetField[]=holiday&facetField[]=technique&facetField[]=cuisine&facetField[]=course&facetField[]=source&facetField[]=brand&facetField[]=difficulty&facetField[]=dish&facetField[]=adtag&solr.view_type=search_internal"
+while True:
+    yummly_url = r"https://mapi.yummly.com/mapi/v13/content/search?q=" + query + r"&start=" + str(current) + r"&maxResult=" + str(rate) + r"&fetchUserCollections=false&allowedContent[]=single_recipe&allowedContent[]=suggested_source&allowedContent[]=suggested_search&allowedContent[]=related_search&facetField[]=diet&facetField[]=holiday&facetField[]=technique&facetField[]=cuisine&facetField[]=course&facetField[]=source&facetField[]=brand&facetField[]=difficulty&facetField[]=dish&facetField[]=adtag&solr.view_type=search_internal"
 
-yummly_raw_json = urllib.request.urlopen(url, timeout = 30).read().decode('utf8')
-yummly = json.loads(accounts_json)
+    try:
+        yummly_raw_json = urllib.request.urlopen(yummly_url, timeout = 30).read().decode('utf8')
+    except HTTPError as err:
+        print(str(err))
+        if "408: Request Timeout" in str(err):
+            print("Retrying...")
+            continue
+        print(yummly_url)
+        sys.exit()
 
-print("Received " + str(len(yummly["feed"])) + " recipes.")
+    try:
+        yummly = json.loads(yummly_raw_json)
+    except JSONDecodeError as err:
+        print(str(err))
+        print(yummly_url)
+        print(page[:300])
+        sys.exit()
 
-recipes = yummly["feed"]
-#[print(x["content"]["details"]["globalId"]) for x in accounts_small]
+    num_recipes_received = len(yummly["feed"])
+    print("Received " + str(num_recipes_received) + " recipes.")
 
-conn = sqlite3.connect("yummly.db")
+    recipes = yummly["feed"]
+    #[print(x["content"]["details"]["globalId"]) for x in accounts_small]
 
-for recipe in recipes:
-    conn.execute("")
+    conn = sqlite3.connect("yummly.db")
+    num_recipes_before = conn.execute("SELECT count(*) FROM Recipe;").fetchone()[0]
+
+    for recipe in recipes:
+        try:
+            r_url = recipe["seo"]["web"]["link-tags"][0]["href"]
+            r_id = recipe["content"]["details"]["globalId"]
+            title = recipe["display"]["displayName"]
+            ingredients = "; ".join([ingredient["ingredient"] for ingredient in recipe["content"]["ingredientLines"]])
+            image_url = recipe["display"]["images"][0]
+            r_description = recipe["seo"]["web"]["meta-tags"]["description"]
+
+            r_json = str(recipe)
+
+            insert_list = [r_id, r_json, title, ingredients, image_url, r_description, r_url]
+
+            conn.execute("INSERT OR IGNORE INTO Recipe VALUES (?,?,?,?,?,?,?)", insert_list)
+        except:
+            e = str(sys.exc_info()[0])
+            print("Error: " + e)
+            print(r_url)
+            continue
+
+    conn.commit()
+
+    num_recipes_after = conn.execute("SELECT count(*) FROM Recipe;").fetchone()[0]
+    print(str(num_recipes_after - num_recipes_before) + " of " + str(num_recipes_received) + " were inserted.")
+
+    conn.close()
+
+    current = current + rate
 
 
-print("Done!")
+
