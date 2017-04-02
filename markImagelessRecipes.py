88107f14b5840272eb0df2aba9abf030b628d03a
Colin Cassady <cvcassady@gmail.com>
2017-04-02 19:32:46 -0400

    converted dict to set

--- markImagelessRecipes.py
+++ markImagelessRecipes.py
@@ -2,22 +2,20 @@ import sqlite3
 import concurrent.futures
 import os
 
-images = os.listdir('images/')
-
-image_dict = {image: 1 for image in images}
+image_set = set(os.listdir('images/'))
 
 conn = sqlite3.connect("yummly.db")
 recipe_ids = conn.execute("SELECT ID FROM Recipe;").fetchall()
 conn.close()
 
-thread_batch = [tuple([tuple(recipe_ids[x*1000:(x+1)*1000]), image_dict]) for x in range(0, (len(recipe_ids)//1000)+1)]
+thread_batch = [tuple([tuple(recipe_ids[x*1000:(x+1)*1000]), image_set]) for x in range(0, (len(recipe_ids)//1000)+1)]
 
 def findMissing(tup):
     conn = sqlite3.connect("yummly.db")
 
     for idx in tup[0]:
         #if (idx[0] + ".jpg" in tup[1]):
-        if (tup[1].get(idx[0] + ".jpg", 0)):
+        if (idx[0] + ".jpg" in tup[1]):
             #print(idx[0] + " in images!")
             conn.execute("UPDATE Recipe SET ImageAvailable=? WHERE ID=?", (1, idx[0]))
     conn.commit()
